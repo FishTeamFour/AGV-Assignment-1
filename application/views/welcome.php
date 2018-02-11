7964453ced24d000fc05e8cb45105e51be68e745
Sergio "Josh" Villegas <joshviba_7@hotmail.com>
2018-02-11 00:30:49 -0800

    Update on Welcome page. closes #32 PHP calls work. Stats now show up and are dynamically loaded. Sets loaded with php calls from JS function

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,84 +4,116 @@
                 document.getElementById("homeLink").classList.add("active");
             }, false);
 </script>
-<div class="outfit">
-    <div id="head">
-        <table>
-            <tr>
-                <td><span style="margin-left: 88px; display: inline-block;"></span></td>
-                <td>
-                    <div style="height: 15px;">
-                    </div>
-                    <div class="dropdown">
-                        <button id="headgear" class="gear dropbtn" style="background-image: url('/assets/images/headgear1.PNG');">Headgear</button>
-                        <div class="dropdown-content">
-                            <a href="#">Option 1</a>
-                            <a href="#">Option 2</a>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
-    <div id="body_hands">
-        <table>
-            <tr>
-                <td>
-                    <div style="height: 35px;">
-                    </div>
-                    <span style="margin-left: 10px; display: inline-block;"></span>
-                    <div class="dropdown">
-                        <button id="weapon" class="gear dropbtn" style="background-image: url('/assets/images/weapon1.PNG');">Weapon</button>
-                        <div class="dropdown-content">
-                            <a href="#">Option 1</a>
-                            <a href="#">Option 2</a>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <div style="height: 10px;">
-                    </div>
-                    <div class="dropdown">
-                        <button id="armor" class="gear-body dropbtn" style="background-image: url('/assets/images/armor1.PNG');">Armour</button>
-                        <div class="dropdown-content">
-                            <a href="#">Option 1</a>
-                            <a href="#">Option 2</a>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <div style="height: 35px;">
-                    </div>
-                    <div class="dropdown">
-                        <button id="offhand" class="gear dropbtn" style="background-image: url('/assets/images/offhand1.PNG');">Offhand</button>
-                        <div class="dropdown-content">
-                            <a href="#">Option 1</a>
-                            <a href="#">Option 2</a>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
-    <div id="feet">
-        <table>
-            <tr>
-                <td><span style="margin-left: 88px; display: inline-block;"></span></td>
-                <td>
-                    <div style="height: 10px;">
-                    </div>
-                    <div class="dropdown">
-                        <button id="footwear" class="gear dropbtn" style="background-image: url('/assets/images/footwear1.PNG');">Footwear</button>
-                        <div class="dropdown-content">
-                            <a href="#">Option 1</a>
-                            <a href="#">Option 2</a>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
-</div>
+<table>
+    <tr>
+        <td style="width: 300px;">
+            <div class="outfit">
+                <div id="head">
+                    <table>
+                        <tr>
+                            <td><span style="margin-left: 88px; display: inline-block;"></span></td>
+                            <td>
+                                <div style="height: 15px;">
+                                </div>
+                                <div class="dropdown">
+                                    <button id="headgear" class="gear dropbtn" style="background-image: url('/{headPath}');"></button>
+                                    <div class="dropdown-content">
+                                        <a href="#">Option 1</a>
+                                        <a href="#">Option 2</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div id="body_hands">
+                    <table>
+                        <tr>
+                            <td>
+                                <div style="height: 35px;">
+                                </div>
+                                <span style="margin-left: 10px; display: inline-block;"></span>
+                                <div class="dropdown">
+                                    <button id="weapon" class="gear dropbtn" style="background-image: url('/{weaponPath}');"></button>
+                                    <div class="dropdown-content">
+                                        <a href="#">Option 1</a>
+                                        <a href="#">Option 2</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div style="height: 10px;">
+                                </div>
+                                <div class="dropdown">
+                                    <button id="armor" class="gear-body dropbtn" style="background-image: url('/{armorPath}');"></button>
+                                    <div class="dropdown-content">
+                                        <a href="#">Option 1</a>
+                                        <a href="#">Option 2</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div style="height: 35px;">
+                                </div>
+                                <div class="dropdown">
+                                    <button id="offhand" class="gear dropbtn" style="background-image: url('/{offhandPath}');"></button>
+                                    <div class="dropdown-content">
+                                        <a href="#">Option 1</a>
+                                        <a href="#">Option 2</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div id="feet">
+                    <table>
+                        <tr>
+                            <td><span style="margin-left: 88px; display: inline-block;"></span></td>
+                            <td>
+                                <div class="dropdown">
+                                    <button id="footwear" class="gear dropbtn" style="background-image: url('/{footwearPath}');"></button>
+                                    <div class="dropdown-content">
+                                        <a href="#">Option 1</a>
+                                        <a href="#">Option 2</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </td>
+        <td>
+            <table>
+                <tr>
+                    <th>
+                        Total Attack: 
+                    </th>
+                    <td>
+                        {atk}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Total Defense: 
+                    </th>
+                    <td>
+                        {def}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Total Health: 
+                    </th>
+                    <td>
+                        {hp}
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 <div>
     <div class="dropdown">
         <button class="dropbtn-set">Select Set</button>
@@ -91,3 +123,4 @@
         </div>
     </div>
 </div>
+<div id="testDiv"></div>
--- changelog.md
+++ changelog.md
@@ -31,3 +31,4 @@ Sam:    Updated css on catalog page
 Sam:    Changed order of catalog headers
 Sam:    Data from csv files now dynamically load into the catalog tables
 Cody:   Fixed Catalog Display/Merge Conflicts locally
+Josh:   Welcome page now loads set with JavaScript function to php code
