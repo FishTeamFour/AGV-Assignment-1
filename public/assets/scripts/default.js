98fbab988796fbbcd9ece92c1829b94f82c5ba5b
Sergio "Josh" Villegas <joshviba_7@hotmail.com>
2018-02-11 17:30:37 -0800

    Welcome page complete. closes #2

--- public/assets/scripts/default.js
+++ public/assets/scripts/default.js
@@ -4,44 +4,163 @@
  * and open the template in the editor.
  */
 
-function selectSet1(){
+function selectSet(id) {
     /*var head = document.getElementById("headgear");
-    var weapon = document.getElementById("weapon");
-    var armor = document.getElementById("armor");
-    var offhand = document.getElementById("offhand");
-    var feet = document.getElementById("footwear");
-    
-    head.style.backgroundImage = "url('/assets/images/headgear1.PNG')";
-    weapon.style.backgroundImage = "url('/assets/images/weapon1.PNG')";
-    armor.style.backgroundImage = "url('/assets/images/armor1.PNG')";
-    offhand.style.backgroundImage = "url('/assets/images/offhand1.PNG')";
-    feet.style.backgroundImage = "url('/assets/images/footwear1.PNG')";*/
-    
+     var weapon = document.getElementById("weapon");
+     var armor = document.getElementById("armor");
+     var offhand = document.getElementById("offhand");
+     var feet = document.getElementById("footwear");
+     
+     head.style.backgroundImage = "url('/assets/images/headgear1.PNG')";
+     weapon.style.backgroundImage = "url('/assets/images/weapon1.PNG')";
+     armor.style.backgroundImage = "url('/assets/images/armor1.PNG')";
+     offhand.style.backgroundImage = "url('/assets/images/offhand1.PNG')";
+     feet.style.backgroundImage = "url('/assets/images/footwear1.PNG')";*/
+
     var host = window.location.hostname;
     var link = "http://";
     host = link.concat(host);
-    host = host.concat("/welcome/getSet/1");
-    
+    host = host.concat("/welcome/getSet/");
+    host = host.concat(id);
+
     window.location = host;
 }
 
-function selectSet2(){
-    /*var head = document.getElementById("headgear");
-    var weapon = document.getElementById("weapon");
-    var armor = document.getElementById("armor");
-    var offhand = document.getElementById("offhand");
-    var feet = document.getElementById("footwear");
+function updateHead(id) {
+    //var head = document.getElementById("headgear").value;
     
-    head.style.backgroundImage = "url('/assets/images/headgear2.PNG')";
-    weapon.style.backgroundImage = "url('/assets/images/weapon2.PNG')";
-    armor.style.backgroundImage = "url('/assets/images/armor2.PNG')";
-    offhand.style.backgroundImage = "url('/assets/images/offhand2.PNG')";
-    feet.style.backgroundImage = "url('/assets/images/footwear2.PNG')";*/
+    var weapon = document.getElementById("weapon").value;
     
+    var armor = document.getElementById("armor").value;
+    
+    var offhand = document.getElementById("offhand").value;
+    
+    var feet = document.getElementById("footwear").value;
+
     var host = window.location.hostname;
     var link = "http://";
     host = link.concat(host);
-    host = host.concat("/welcome/getSet/2");
+    host = host.concat("/welcome/updateItem/");
+    host = host.concat(id);
+    host = host.concat("-");
+    host = host.concat(weapon);
+    host = host.concat("-");
+    host = host.concat(armor);
+    host = host.concat("-");
+    host = host.concat(offhand);
+    host = host.concat("-");
+    host = host.concat(feet);
+
+    window.location = host;
+}
+
+function updateWeapon(id) {
+    var head = document.getElementById("headgear").value;
+    //var weapon = document.getElementById("weapon");
     
+    var armor = document.getElementById("armor").value;
+    
+    var offhand = document.getElementById("offhand").value;
+    
+    var feet = document.getElementById("footwear").value;
+
+    var host = window.location.hostname;
+    var link = "http://";
+    host = link.concat(host);
+    host = host.concat("/welcome/updateItem/");
+    host = host.concat(head);
+    host = host.concat("-");
+    host = host.concat(id);
+    host = host.concat("-");
+    host = host.concat(armor);
+    host = host.concat("-");
+    host = host.concat(offhand);
+    host = host.concat("-");
+    host = host.concat(feet);
+
+    window.location = host;
+}
+
+function updateArmor(id) {
+    var head = document.getElementById("headgear").value;
+    
+    var weapon = document.getElementById("weapon").value;
+    
+    //var armor = document.getElementById("armor").value;
+    
+    var offhand = document.getElementById("offhand").value;
+    
+    var feet = document.getElementById("footwear").value;
+
+    var host = window.location.hostname;
+    var link = "http://";
+    host = link.concat(host);
+    host = host.concat("/welcome/updateItem/");
+    host = host.concat(head);
+    host = host.concat("-");
+    host = host.concat(weapon);
+    host = host.concat("-");
+    host = host.concat(id);
+    host = host.concat("-");
+    host = host.concat(offhand);
+    host = host.concat("-");
+    host = host.concat(feet);
+
+    window.location = host;
+}
+
+function updateOffhand(id) {
+    var head = document.getElementById("headgear").value;
+    
+    var weapon = document.getElementById("weapon").value;
+    
+    var armor = document.getElementById("armor").value;
+    
+    //var offhand = document.getElementById("offhand").value;
+    
+    var feet = document.getElementById("footwear").value;
+
+    var host = window.location.hostname;
+    var link = "http://";
+    host = link.concat(host);
+    host = host.concat("/welcome/updateItem/");
+    host = host.concat(head);
+    host = host.concat("-");
+    host = host.concat(weapon);
+    host = host.concat("-");
+    host = host.concat(armor);
+    host = host.concat("-");
+    host = host.concat(id);
+    host = host.concat("-");
+    host = host.concat(feet);
+
+    window.location = host;
+}
+
+function updateFeet(id) {
+    var head = document.getElementById("headgear").value;
+    
+    var weapon = document.getElementById("weapon").value;
+    
+    var armor = document.getElementById("armor").value;
+    
+    var offhand = document.getElementById("offhand").value;
+    
+    //var feet = document.getElementById("footwear").value;
+
+    var host = window.location.hostname;
+    var link = "http://";
+    host = link.concat(host);
+    host = host.concat("/welcome/updateItem/");
+    host = host.concat(head);
+    host = host.concat("-");
+    host = host.concat(weapon);
+    host = host.concat("-");
+    host = host.concat(armor);
+    host = host.concat("-");
+    host = host.concat(offhand);
+    host = host.concat("-");
+    host = host.concat(id);
+
     window.location = host;
 }
